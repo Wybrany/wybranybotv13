bad84909c4da6c00075dee528ef3ea98b59e5169
Jonatan Viitala <Jonatan34@gmail.com>
2021-08-13 23:34:14 +0200

    Testing some stuff

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import dotenv from "dotenv";
+import { Client, Intents } from "discord.js";
+
+dotenv.config();
+const discord_token = process.env.TOKEN as string;
+if(!discord_token) {console.log(`No "TOKEN" was submitted as a discord token. Now exiting`); process.exit(0)}
+
+const client = new Client({intents: [Intents.FLAGS.GUILDS]});
+
+client.login(process.env.TOKEN);
